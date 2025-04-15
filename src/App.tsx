88f6d7edceb40378541390963c87d812a03cc3b9
Kasam Aftab <ka276310@gmail.com>
2025-04-15 22:09:44 +0100

    DEMO

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Layout } from "./components/Layout";
+import { Hero } from "./components/sections/Hero";
+
+function App() {
+  return (
+    <Layout title="LandingPageAI">
+      {" "}
+      <Hero />
+    </Layout>
+  );
+}
+
+export default App;
